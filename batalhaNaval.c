193a1c6c9ab4e775eda113013c37fb74d16043b2
Ítalo Araújo Silva Acioly <142676525+ItaloDevOficial@users.noreply.github.com>
2025-03-21 10:00:18 -0300

    finalizando o jogo adicionando as habilidades para a batalha naval

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,45 +1,102 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void inicializarTabuleiro(int tabuleiro[10][10]) {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            tabuleiro[i][j] = 0; // 0 representa água
+#define TAMANHO_TABULEIRO 10
+//#define TAMANHO_NAVIO 3
+#define AGUA 0
+//#define NAVIO 3
+#define HABILIDADE 5
+
+void inicializarTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            tabuleiro[i][j] = AGUA;
         }
     }
 }
 
-int posicionarNavio(int tabuleiro[10][10], int linha, int coluna, int horizontal) {
+int validarPosicao(int linha, int coluna, int tamanho, int horizontal) {
     if (horizontal) {
-        if (coluna + 3 > 10) return 0;
-        for (int i = 0; i < 3; i++) {
-            if (tabuleiro[linha][coluna + i] != 0) return 0;
-        }
-        for (int i = 0; i < 3; i++) {
-            tabuleiro[linha][coluna + i] = 3;
-        }
+        return (coluna + tamanho <= TAMANHO_TABULEIRO);
     } else {
-        if (linha + 3 > 10) return 0;
-        for (int i = 0; i < 3; i++) {
-            if (tabuleiro[linha + i][coluna] != 0) return 0;
-        }
-        for (int i = 0; i < 3; i++) {
-            tabuleiro[linha + i][coluna] = 3;
-        }
+        return (linha + tamanho <= TAMANHO_TABULEIRO);
+    }
+}
+
+/* int posicionarNavio(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna, int horizontal) {
+    if (!validarPosicao(linha, coluna, TAMANHO_NAVIO, horizontal)) return 0;
+    
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        if (tabuleiro[linha + (horizontal ? 0 : i)][coluna + (horizontal ? i : 0)] != AGUA) return 0;
+    }
+    
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[linha + (horizontal ? 0 : i)][coluna + (horizontal ? i : 0)] = NAVIO;
     }
     return 1;
+}*/
+
+/* int posicionarNavioDiagonal(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna, int direcao) {
+    if ((direcao == 1 && (linha + TAMANHO_NAVIO > TAMANHO_TABULEIRO || coluna + TAMANHO_NAVIO > TAMANHO_TABULEIRO)) ||
+        (direcao == 0 && (linha + TAMANHO_NAVIO > TAMANHO_TABULEIRO || coluna - (TAMANHO_NAVIO - 1) < 0))) {
+        return 0;
+    }
+    
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        if (tabuleiro[linha + i][coluna + (direcao ? i : -i)] != AGUA) return 0;
+    }
+    
+    for (int i = 0; i < TAMANHO_NAVIO; i++) {
+        tabuleiro[linha + i][coluna + (direcao ? i : -i)] = NAVIO;
+    }
+    return 1;
+} */
+
+void aplicarHabilidade(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO], int linha, int coluna, int tipo) {
+    if (tipo == 1) { // Cone
+        for (int i = 0; i < 3; i++) {
+            for (int j = -i; j <= i; j++) {
+                if (linha + i < TAMANHO_TABULEIRO && coluna + j >= 0 && coluna + j < TAMANHO_TABULEIRO) {
+                    if (tabuleiro[linha + i][coluna + j] == AGUA) {
+                        tabuleiro[linha + i][coluna + j] = HABILIDADE;
+                    }
+                }
+            }
+        }
+    } else if (tipo == 2) { // Cruz
+        for (int i = -2; i <= 2; i++) {
+            if (linha + i >= 0 && linha + i < TAMANHO_TABULEIRO) {
+                if (tabuleiro[linha + i][coluna] == AGUA) {
+                    tabuleiro[linha + i][coluna] = HABILIDADE;
+                }
+            }
+            if (coluna + i >= 0 && coluna + i < TAMANHO_TABULEIRO) {
+                if (tabuleiro[linha][coluna + i] == AGUA) {
+                    tabuleiro[linha][coluna + i] = HABILIDADE;
+                }
+            }
+        }
+    } else if (tipo == 3) { // Octaedro
+        for (int i = -2; i <= 2; i++) {
+            for (int j = -2 + abs(i); j <= 2 - abs(i); j++) {
+                if (linha + i >= 0 && linha + i < TAMANHO_TABULEIRO && coluna + j >= 0 && coluna + j < TAMANHO_TABULEIRO) {
+                    if (tabuleiro[linha + i][coluna + j] == AGUA) {
+                        tabuleiro[linha + i][coluna + j] = HABILIDADE;
+                    }
+                }
+            }
+        }
+    }
 }
 
-void exibirTabuleiro(int tabuleiro[10][10]) {
+void exibirTabuleiro(int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO]) {
     printf("     a  b  c  d  e  f  g  h  i  j\n");
     printf("   ---------------------------------");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < TAMANHO_TABULEIRO; i++) {
         printf("\n%2d |", i + 1);
-        for (int j = 0; j < 10; j++) {
-            if (tabuleiro[i][j] == 0) {
-                printf(" ~ "); // Água representada por '~'
-            } else {
-                printf(" # "); // Navio representado por '#'
-            }
+        for (int j = 0; j < TAMANHO_TABULEIRO; j++) {
+            char simbolo = tabuleiro[i][j] == AGUA ? '~' : (tabuleiro[i][j] == HABILIDADE ? '#' : '*');
+            printf(" %c ", simbolo);
         }
         printf("|");
     }
@@ -48,11 +105,12 @@ void exibirTabuleiro(int tabuleiro[10][10]) {
 }
 
 int main() {
-    int tabuleiro[10][10];
+    int tabuleiro[TAMANHO_TABULEIRO][TAMANHO_TABULEIRO];
     inicializarTabuleiro(tabuleiro);
     
-    posicionarNavio(tabuleiro, 2, 3, 1);
-    posicionarNavio(tabuleiro, 5, 6, 0);
+    aplicarHabilidade(tabuleiro, 6, 2, 1); // Cone
+    aplicarHabilidade(tabuleiro, 6, 7, 2); // Cruz
+    aplicarHabilidade(tabuleiro, 2, 2, 3); // Octaedro
     
     exibirTabuleiro(tabuleiro);
     
